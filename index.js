bf2fc3fe7a534b758ae2642d519447505ca72e6f
Michael Vasquez <michael_s_vasquez@yahoo.com>
2024-03-26 23:05:33 -0500

    practice

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function render(leads) {
 }
 
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
+    localStorage.removeItem(myLeads)
     myLeads = []
     render(myLeads)
 })
